e6cfd794b49b3076bbb84b4876d65955db76f6fc
David Pérez González <david.perez.gonzalez@rwth-aachen.de>
2021-08-02 14:37:23 +0200

    New function to clean the code and general comments.

--- LUT/LUT.py
+++ LUT/LUT.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
+__author__ = 'david.perez.gonzalez'
 """
-Created on Thu Dec 17 17:29:28 2020
-
-@author: David
+LUT.py provides a frame to create the look up tables and store
+the information about the closest peaks to each grid point.
 """
 import cPickle as pickle
 import os
@@ -18,28 +18,16 @@ class LUD():
         # self.precision_grid = 2
         self.decimals = decimals # 0.01
         self.HVD_list = ["000", "010", "100", "111"]
-        self.val_region_HVD_list = [0.05, 0.05, 0.05, 0.05] #[0.2, 0.2, 0.2, 0.3]
+        self.val_region_HVD_list = [0.1, 0.1, 0.1, 0.1] #[0.2, 0.2, 0.2, 0.3]
         self.n_roi = [36, 30, 30, 25]
         self.selected_HVD = int(selected_HVD)
         self.pathtodirectory = pathtodirectory
         self.init_event = init_event
         self.final_event = final_event
-        # pathtodirectory = '/media/david.perez/pet-scratch/Measurements/Hypmed/2021-02-17_-_15-20-29_-_HypmedStacks/2021-02-17_-_15-20-39_-_2011002000_A41B0821-034_2021-02-05/2021-02-17_-_16-17-01_-_floodmapWithSources/ramdisks_2021-02-17_-_16-37-36/'
-        # self.pathtodirectory = '/media/david.perez/pet-scratch/Measurements/Hypmed/2021-02-17_-_15-20-29_-_HypmedStacks/2021-03-01_-_13-29-22_-_2011002000_A41B069400001_2021-02-25/2021-03-01_-_16-27-02_-_floodmapWithSources2/ramdisks_2021-03-01_-_16-53-55/'
-        # self.pathtodirectory = '/media/david.perez/pet-scratch/Measurements/Hypmed/2021-02-17_-_15-20-29_-_HypmedStacks/2021-03-12_-_15-42-31_-_2010002165_A41B0821-015_2021-03-08/2021-03-15_-_12-30-54_-_floodmapWithSources/ramdisks_2021-03-15_-_13-06-48/'
 
         self.pathtodirectoryReadLUD = 'dic-LUD/'
-        self.pathtodirectoryReadHDF5 = 'hdf5Data/'
+        # self.pathtodirectoryReadHDF5 = 'hdf5Data/'
         self.pathtodirectoryCheck = 'dic-checked/'
-
-        # pathtodirectoryReadLUD = '20210304_NEW-2021-02-17_dic-LUD/'
-        # pathtodirectoryReadHDF5 = '20210302_NEW-2021-02-17_hdf5Data/'
-        # self.pathtodirectoryReadLUD = '20210303_NEW-2021-02-17_dic-LUD/'
-        # self.pathtodirectoryReadHDF5 = '20210302_NEW-2021-02-17_hdf5Data/'
-        # self.pathtodirectoryCheck = '20210303_NEW-2021-02-17_dic-checked/'
-        # pathtodirectoryReadLUD = '20210315_NEW_dic-LUD/'
-        # pathtodirectoryReadHDF5 = '20210315_NEW_hdf5Data/'
-        # self.pathtodirectoryCheck = '20210315_NEW_dic-checked/'
 
         self.pathtodirectoryRead = self.pathtodirectory + self.pathtodirectoryCheck
 
@@ -162,40 +150,12 @@ class LUD():
             for j in np.arange(-24,24+decimals,decimals):
                 p_clo, l_d, cry, p_clo_2, l_d_2, cry_2 = self.__closest_peak((round(i,precision_grid),round(j,precision_grid)),dic_crystal)
                 into = False
-                if l_d < 0: #1: now set to zero to deactivate Hull
+                if l_d < 1: #0: now set to one to activate Hull
                     for cH in hull_list:
                         inside = self.__in_hull(np.array([[round(i,precision_grid),round(j,precision_grid)]]), cH)
 
                         if inside[0]:
-                            # plt.title('in hull')
-                            # plt.plot(cH.points[:, 0], cH.points[:, 1], 'o', hold=1)
-                            #
-                            # # plot the convex hull
-                            # edges = set()
-                            # edge_points = []
-                            #
-                            # def add_edge(i, j):
-                            #     """Add a line between the i-th and j-th points, if not in the list already"""
-                            #     if (i, j) in edges or (j, i) in edges:
-                            #         # already added
-                            #         return
-                            #     edges.add((i, j))
-                            #     edge_points.append(cH.points[[i, j]])
-                            #
-                            # for ia, ib in cH.convex_hull:
-                            #     add_edge(ia, ib)
-                            #
-                            # lines = LineCollection(edge_points, color='g')
-                            # plt.gca().add_collection(lines)
-
                             into = True
-
-                            # plt.plot(np.array([[round(i, 1), round(j, 1)]])[inside, 0],
-                            #          np.array([[round(i, 1), round(j, 1)]])[inside, 1], '.k')
-                            #
-                            # plt.plot(np.array([[round(i, 1), round(j, 1)]])[np.where(inside == False), 0],
-                            #          np.array([[round(i, 1), round(j, 1)]])[np.where(inside == False), 1], '.r')
-                            # plt.show()
                             break
                 if l_d < val_region or into:
                     if l_d_2 != 0:
@@ -217,14 +177,12 @@ class LUD():
                         dic_label["QF"] = QF
                         lud[round(i, precision_grid), round(j, precision_grid)] = dic_label
                         dic_label = {}
-                        print(round(i,precision_grid),round(j,precision_grid))
-                        print(l_d)
-                        print("l_2", l_d_2)
-                        print(lud[round(i, precision_grid), round(j, precision_grid)] )
-
+                        # print(round(i,precision_grid),round(j,precision_grid))
+                        # print(l_d)
+                        # print("l_2", l_d_2)
+                        # print(lud[round(i, precision_grid), round(j, precision_grid)] )
                 else:
                     lud[round(i, precision_grid), round(j, precision_grid)] = None
-
                 n_lud += 1
             #i_lud += 1
         return lud
