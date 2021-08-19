1a4fb24a666d2b9c41b1272469efb1de66b78bac
Janko <janko.lambertus@pmi.rwth-aachen.de>
2021-08-19 18:15:37 +0200

    ReadDebugSInglesCRT_Val.py debugging

--- CreateData/ReadDebugSinglesCRT_Val.py
+++ CreateData/ReadDebugSinglesCRT_Val.py
@@ -153,18 +153,18 @@ def __save_data(folder, dic_HVD, name):
     with open('{}/{}.pickle'.format(folder, name), 'wb') as handle:
         pickle.dump(dic_HVD, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
-parser = argparse.ArgumentParser()
+# parser = argparse.ArgumentParser()
 # parser.add_argument('--fileType', dest='fType', help='Specifiy which type of file to be read', default='.DebugCoincidentSingles')
 # parser.add_argument('--stackID', dest='sID', help='Specifiy the stackID to be read')
-parser.add_argument('--fileDirectory', dest='fileDirect', help='Specifiy the name of the   \
-                                                 directory where to read the files from')
-parser.add_argument('--saveDirectory', dest='saveDirect', help='Specifiy the name of the   \
-                                                 directory where to save the files')
-
-args = parser.parse_args()
+# parser.add_argument('--fileDirectory', dest='fileDirect', help='Specifiy the name of the   \
+#                                                  directory where to read the files from')
+# parser.add_argument('--saveDirectory', dest='saveDirect', help='Specifiy the name of the   \
+#                                                  directory where to save the files')
+#
+# args = parser.parse_args()
 # file_type, pathtodirectoryRead = args.fType, args.fileDirect
-pathtodirectoryRead, pathtodirectorySave = args.fileDirect, args.saveDirect
-
+# pathtodirectoryRead, pathtodirectorySave = args.fileDirect, args.saveDirect
+pathtodirectoryRead, pathtodirectorySave = "/media/janko.lambertus/pet-scratch/Measurements/Hypmed/2021-02-17_-_15-20-29_-_HypmedStacks/2021-03-12_-_15-42-31_-_2010002165_A41B0821-015_2021-03-08/2021-03-15_-_12-30-54_-_floodmapWithSources/", "/media/janko.lambertus/pet-scratch/Janko/Master/Data/CIA_FT/Test3/"
 list_save_crt = {"stack_id": [2,3],
                  "dicval_000": [192, 193], "dicval_100": [193, 194], "dicval_010": [194, 195], "dicval_111": [199, 200]}
 stack_id = {}
@@ -216,6 +216,7 @@ stack_id_coinc = list_save_dic_crt['stack_id'][1]
 
 for cluster in list_save_dic_crt['stack_id'].keys():
     if list_save_dic_crt['stack_id'][cluster] == stack_id_cal:
+        print(cluster)
         cogRef_cal.append([int(list_save_dic_crt["dicval_000"][cluster][0]), int(list_save_dic_crt["dicval_100"][cluster][0]),
                        int(list_save_dic_crt["dicval_010"][cluster][0]), int(list_save_dic_crt["dicval_111"][cluster][0])])
 
